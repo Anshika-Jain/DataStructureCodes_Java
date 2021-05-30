a10ccc2891d5ef5d95ed6d8c7e0ebe750bbb02c4
Anshika Jain <anshikajain604@gmail.com>
2021-05-30 19:19:39 +0530

    Add files via upload

---
+++ linked list/prac.cpp
@@ -0,0 +1,16 @@
+#include <iostream>;
+using namespace std;
+
+bool threeSum(int arr[], int n, int x){
+    int found = false;
+    
+    return found;
+}
+
+int main(){
+    int arr[] = {12, 3, 4, 1, 6, 9};
+    int n = 6;
+    int sum = 24;
+    bool found = threeSum(arr,n,sum);
+    return 0;
+}
