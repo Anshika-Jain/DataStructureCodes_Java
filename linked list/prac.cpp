623f8744cb5654b6a64d260ea00dad47eccd1cfe
Anshika Jain <anshikajain604@gmail.com>
2021-05-30 19:20:52 +0530

    Update prac.cpp

--- linked list/prac.cpp
+++ linked list/prac.cpp
@@ -1,16 +1,51 @@
-#include <iostream>;
+//Linked List Implementation without defining a class
+#include <iostream>
 using namespace std;
 
-bool threeSum(int arr[], int n, int x){
-    int found = false;
+class Node{
+    public:
+    int data;
+    Node* next;
+        
+    Node(int val){
+        data = val;
+        next = NULL;
+    }
+};
+
+void insertAtEnd(Node* &head, int  val){
+    Node* n = new Node(val);
+    if(head == NULL){
+        head = n;
+        return;
+    }
     
-    return found;
+    Node* temp = head;
+    while(temp->next != NULL){
+        temp = temp->next;
+    }
+    temp->next = n;
 }
 
+void display(Node* head){
+    Node* temp = head;
+    while(temp != NULL){
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+    cout<<endl;
+}
+
+
+    
+
 int main(){
-    int arr[] = {12, 3, 4, 1, 6, 9};
-    int n = 6;
-    int sum = 24;
-    bool found = threeSum(arr,n,sum);
+    
+    Node* head = NULL;
+    insertAtEnd(head,2);
+    insertAtEnd(head,4);
+    insertAtEnd(head,7);
+    display(head); // 2 4 7
+    
     return 0;
 }
